78292a1d8c0f88bb64583868244d451c4484b1ef
YOUR NAMEGISMOJU <JU.DERVILLE@GMAIL.COM>
2016-04-07 08:27:02 +0200

    extension
    extension

--- popup.js
+++ popup.js
@@ -1,14 +1,32 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
 
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
+// chrome.history.search({text: '', maxResults: 10}, function(data) {
+//    data.forEach(function(page) {
+//        console.log(page.url);
+//    });
+//});
 
+function onAnchorClick(event) {
+  chrome.tabs.create({ url: event.srcElement.href });
+  return false;
+}
 
-//example of using a message handler from the inject scripts
-//chrome.extension.onMessage.addListener(
- // function(request, sender, sendResponse) {
- // 	chrome.pageAct
-//ion.show(sender.tab.id);
-  //  sendResponse();
-  //});
+// Given an array of URLs, build a DOM list of these URLs in the
+// browser action popup.
+function buildPopupDom(mostVisitedURLs) {
+  var popupDiv = document.getElementById('mostVisited_div');
+  var ol = popupDiv.appendChild(document.createElement('ol'));
+
+  for (var i = 0; i < mostVisitedURLs.length; i++) {
+    var li = ol.appendChild(document.createElement('li'));
+    li.href = mostVisitedURLs[i].url;
+    li.appendChild(document.createTextNode(mostVisitedURLs[i].title));
+    li.addEventListener('click', onAnchorClick);
+  }
+}
+
+chrome.topSites.get(buildPopupDom);
+
+//chrome.history.addUrl(object details, function callback)
+chrome.history.addUrl("historique", function {
+  
+})
